f1dcf75ab14aafc2bb6d91a05ee46891d70d3e1b
NicoleDVH <nicole12909@gmail.com>
2017-07-03 22:06:00 -0400

    Created comment form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
 	
 	def create
-		@post = Post.find(params[:id])
-		@comment = @post.comment.create(parans[:comment.permit(:name, :body)])
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.create(params[:comment].permit(:name, :body))
 		
 		redirect_to post_path(@post)
 	end
