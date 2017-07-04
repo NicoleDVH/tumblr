4cf1992cce848497a08d1965fa9e6b6ef81b198d
NicoleDVH <nicole12909@gmail.com>
2017-07-03 21:52:59 -0400

    Created comments nested in posts

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,9 @@
+class CommentsController < ApplicationController
+	def create
+		@post = Post.find(params[:id])
+		@comment = @post.comment.create(parans[:comment.permit(:name, :body)])
+		
+		redirect_to post_path(@post)
+	end
+
+end
